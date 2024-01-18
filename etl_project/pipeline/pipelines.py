a42c621ef0ba6742f896186f7e5b07e186b446c4
Gabrielle Klimovitsky <gabrielle@msk.ai>
2024-01-18 12:28:14 +0000

    adding logging

--- etl_project/pipeline/pipelines.py
+++ etl_project/pipeline/pipelines.py
@@ -4,10 +4,12 @@
 import pandas as pd
 import psycopg2
 #from sqlalchemy import create_engine, Table, Column, String, Integer, MetaData, ForeignKey, Boolean
+from etl_project.assets.pipeline_logging import PipelineLogging
+from etl_project.assets.metadata_logging import MetaDataLogging, MetaDataLoggingStatus
 from sqlalchemy import create_engine, Table, Column, String, Integer, MetaData, ForeignKey, Boolean
 from sqlalchemy import DateTime
 import datetime
-
+import yaml
 
 
 
@@ -29,211 +31,267 @@ from etl_project.assets.spotify_assets import (
 from etl_project.connectors.postgresql import PostgreSqlClient
 from etl_project.connectors.spotify_api import SpotifyAPI
 from sqlalchemy import Table, MetaData, Column, Integer, String, Float
-
+from pathlib import Path
 
 
 
 if __name__ == "__main__":
-    load_dotenv()
-    CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
-    CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
-    DB_USERNAME = os.environ.get("DB_USERNAME")
-    DB_PASSWORD = os.environ.get("DB_PASSWORD")
-    SERVER_NAME = os.environ.get("SERVER_NAME")
-    DATABASE_NAME = os.environ.get("DATABASE_NAME")
-    PORT=os.environ.get("PORT")
     
-    #sp = init_spotify_client(CLIENT_ID, CLIENT_SECRET)
-    spotif_client=SpotifyAPI(CLIENT_ID,CLIENT_SECRET)
-
-    # Connect to the PostgreSQL server (default database)
-    conn = psycopg2.connect(
-            host=SERVER_NAME,
-            user=DB_USERNAME,
-            password=DB_PASSWORD,
-            dbname="postgres",  # Connect to the default database
-            port=PORT
+    yaml_file_path = __file__.replace(".py", ".yaml")
+    if Path(yaml_file_path).exists():
+        with open(yaml_file_path) as yaml_file:
+            pipeline_config = yaml.safe_load(yaml_file)
+            config = pipeline_config.get("config")
+            PIPELINE_NAME = pipeline_config.get("name")
+    else:
+        raise Exception(
+            f"Missing {yaml_file_path} file! Please create the yaml file with at least a `name` key for the pipeline name."
         )
-    conn.autocommit = True
-    cursor = conn.cursor()
 
-    # Check if the target database exists
-    cursor.execute(f"SELECT 1 FROM pg_catalog.pg_database WHERE datname = '{DATABASE_NAME}'")
-    exists = cursor.fetchone()
-    if not exists:
-        # Create the database if it doesn't exist
-        cursor.execute(f"CREATE DATABASE {DATABASE_NAME}")
-
-    # Close the connection to the default database
-    cursor.close()
-    conn.close()
-    
-    # Load the CSV file
-    file_path="etl_project/data/top_artist_from_global50.csv"
-
-    df = pd.read_csv(file_path)
-
-    artists_id = df['artist_id']
-
-    #### artist ###
-    
-    artist_data=extract_artists(spotif_client, artists_id)
-
-
-    artist_df = transform_artists(artist_data)
-
-    postgresql_client = PostgreSqlClient(
-        server_name=SERVER_NAME,
-        database_name=DATABASE_NAME,
-        username=DB_USERNAME,
-        password=DB_PASSWORD,
-        port=PORT,
+    pipeline_logging = PipelineLogging(
+    pipeline_name=pipeline_config.get("name"),
+    log_folder_path=config.get("log_folder_path"),
     )
+    LOGGING_SERVER_NAME = os.environ.get("LOGGING_SERVER_NAME")
+    LOGGING_DATABASE_NAME = os.environ.get("LOGGING_DATABASE_NAME")
+    LOGGING_USERNAME = os.environ.get("LOGGING_USERNAME")
+    LOGGING_PASSWORD = os.environ.get("LOGGING_PASSWORD")
+    LOGGING_PORT = os.environ.get("LOGGING_PORT")
+    postgresql_logging_client = PostgreSqlClient(
+        server_name=LOGGING_SERVER_NAME,
+        database_name=LOGGING_DATABASE_NAME,
+        username=LOGGING_USERNAME,
+        password=LOGGING_PASSWORD,
+        port=LOGGING_PORT,
+    )
+    metadata_logger = MetaDataLogging(
+        pipeline_name=PIPELINE_NAME,
+        postgresql_client=postgresql_logging_client,
+        config=pipeline_config.get("config"),
+    )
+    try:
+             
+        metadata_logger.log()  # log start
+        pipeline_logging.logger.info("Starting pipeline run")
+        pipeline_logging.logger.info("Getting pipeline environment variables")
+        load_dotenv()
+        CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
+        CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
+        DB_USERNAME = os.environ.get("DB_USERNAME")
+        DB_PASSWORD = os.environ.get("DB_PASSWORD")
+        SERVER_NAME = os.environ.get("SERVER_NAME")
+        DATABASE_NAME = os.environ.get("DATABASE_NAME")
+        PORT=os.environ.get("PORT")
+
+        pipeline_logging.logger.info("Initializing Spotify Client")
+        spotif_client=SpotifyAPI(CLIENT_ID,CLIENT_SECRET)
+
+        # Connect to the PostgreSQL server (default database)
+        conn = psycopg2.connect(
+                host=SERVER_NAME,
+                user=DB_USERNAME,
+                password=DB_PASSWORD,
+                dbname="postgres",  # Connect to the default database
+                port=PORT
+            )
+        conn.autocommit = True
+        cursor = conn.cursor()
+
+        # Check if the target database exists
+        cursor.execute(f"SELECT 1 FROM pg_catalog.pg_database WHERE datname = '{DATABASE_NAME}'")
+        exists = cursor.fetchone()
+        if not exists:
+            # Create the database if it doesn't exist
+            cursor.execute(f"CREATE DATABASE {DATABASE_NAME}")
+
+        # Close the connection to the default database
+        cursor.close()
+        conn.close()
+
+        # Load the CSV file
+        file_path="etl_project/data/top_artist_from_global50.csv"
+
+        df = pd.read_csv(file_path)
+
+        artists_id = df['artist_id']
+
+        #### artist ###
+        pipeline_logging.logger.info("Extracting artist data from Spotify API")
+        artist_data=extract_artists(spotif_client, artists_id)
+
+        pipeline_logging.logger.info("Transforming artist data")
+        artist_df = transform_artists(artist_data)
+
+        postgresql_client = PostgreSqlClient(
+            server_name=SERVER_NAME,
+            database_name=DATABASE_NAME,
+            username=DB_USERNAME,
+            password=DB_PASSWORD,
+            port=PORT,
+        )
 
 
-    metadata = MetaData()
-    artist_table = Table(
-        "artists",
-        metadata,
+        metadata = MetaData()
+        artist_table = Table(
+            "artists",
+            metadata,
+            Column('artist_id', String),
+            Column('name', String),
+            Column('popularity', Integer),
+            Column('genres', String),
+            Column('followers', Integer),
+            Column('spotify_url', String),
+            Column('load_date', DateTime, default=datetime.datetime.utcnow),
+           # schema='raw'
+        )
+
+
+        pipeline_logging.logger.info("Loading transformed artist data to Spoty database")
+        load(
+            df=artist_df,
+            postgresql_client=postgresql_client,
+            table=artist_table,
+            metadata=metadata,
+            load_method="insert",
+        )
+
+
+        #### tracks ###
+        pipeline_logging.logger.info("Extracting tracks data from Spotify API")
+
+        market=["US"]
+
+        track_data=extract_top_tracks(spotif_client,artists_id,market)
+
+        pipeline_logging.logger.info("Transforming tracks data")
+
+        top_track_df = transform_tracks(track_data)
+
+
+        print(top_track_df)
+
+        tracks_table = Table(
+            'tracks', metadata,
+            Column('track_id', String),
+            Column('name', String),
+            Column('artist_id', String),
+            Column('album_id', String),
+            Column('duration_ms', Integer),
+            Column('popularity', Integer),
+            Column('explicit', Boolean),
+            Column('preview_url', String),
+            Column('track_number', Integer),
+            Column('market', String),
+            Column('load_date', DateTime, default=datetime.datetime.utcnow),
+             #schema='raw'
+        )
+
+        pipeline_logging.logger.info("Loading transformed tracks data to Spoty database")
+
+        load(
+            df=top_track_df,
+            postgresql_client=postgresql_client,
+            table=tracks_table,
+            metadata=metadata,
+            load_method="insert",
+        )
+
+
+
+
+
+
+
+
+        #### albums ###
+
+        # Extract albums
+        pipeline_logging.logger.info("Extracting albums data from Spotify API")
+
+        album_data = extract_albums(spotif_client, artists_id)
+
+        # Transform album data
+        pipeline_logging.logger.info("Transforming albums data")
+
+        album_df = transform_albums(album_data)
+        #print(album_df)
+
+
+
+        albums_table = Table(
+            'albums', metadata,
+            Column('album_id', String),
+            Column('album_name', String),
+            Column('album_type', String),
+            Column('artist_id', String),
+            Column('release_date', String),
+            Column('total_tracks', Integer),
+            Column('load_date', DateTime, default=datetime.datetime.utcnow),
+             #schema='raw'
+            #schema?
+        )
+
+        # Load album data into the database
+        pipeline_logging.logger.info("Loading transformed albums data to Spoty database")
+
+        load(
+            df=album_df,
+            postgresql_client=postgresql_client,
+            table=albums_table,
+            metadata=metadata,
+            load_method="insert",
+        )
+
+
+
+
+        """
+
+        playlist_dict = {
+            "SE": "7jmQBEvJyGHPqKEl5UcEe9",
+            "GB": "37i9dQZEVXbLnolsZ8PSNw",
+            "BE": "37i9dQZEVXbJNSeeHswcKB",
+            "USA":"37i9dQZEVXbLRQDuF5jeBp",
+            "PT":"37i9dQZEVXbKyJS56d1pgi"
+        }
+
+        """
+
+        pipeline_logging.logger.info("Extracting playlist data from Spotify API")
+
+        playlist_dict = {
+            "SE": "7jmQBEvJyGHPqKEl5UcEe9"
+        }
+        playlist_data = extract_global_playlist_countries(spotif_client,playlist_dict)
+        # print(playlist_data)  
+        pipeline_logging.logger.info("Transforming playlist data")
+
+        df_playlist = transform_playlist(playlist_data)
+
+
+        playlist_global = Table(
+        'playlist', metadata,
         Column('artist_id', String),
-        Column('name', String),
-        Column('popularity', Integer),
-        Column('genres', String),
-        Column('followers', Integer),
-        Column('spotify_url', String),
-        Column('load_date', DateTime, default=datetime.datetime.utcnow),
-       # schema='raw'
-    )
+        Column('playlist_country', String))
 
+        pipeline_logging.logger.info("Loading transformed playlists data to Spoty database")
+        load(
+            df=df_playlist,
+            postgresql_client=postgresql_client,
+            table=playlist_global,
+            metadata=metadata,
+            load_method="insert",
+        )
 
-
-    load(
-        df=artist_df,
-        postgresql_client=postgresql_client,
-        table=artist_table,
-        metadata=metadata,
-        load_method="insert",
-    )
-
-
-    #### tracks ###
-
-    market=["US"]
-
-    track_data=extract_top_tracks(spotif_client,artists_id,market)
-
-
-    top_track_df = transform_tracks(track_data)
-
-
-    print(top_track_df)
-
-    tracks_table = Table(
-        'tracks', metadata,
-        Column('track_id', String),
-        Column('name', String),
-        Column('artist_id', String),
-        Column('album_id', String),
-        Column('duration_ms', Integer),
-        Column('popularity', Integer),
-        Column('explicit', Boolean),
-        Column('preview_url', String),
-        Column('track_number', Integer),
-        Column('market', String),
-        Column('load_date', DateTime, default=datetime.datetime.utcnow),
-         #schema='raw'
-    )
-
-
-    load(
-        df=top_track_df,
-        postgresql_client=postgresql_client,
-        table=tracks_table,
-        metadata=metadata,
-        load_method="insert",
-    )
-
-
-
-
-
-
-    
-
-    #### albums ###
-    
-    # Extract albums
-    album_data = extract_albums(spotif_client, artists_id)
-
-    # Transform album data
-    album_df = transform_albums(album_data)
-    #print(album_df)
-
-
-
-    albums_table = Table(
-        'albums', metadata,
-        Column('album_id', String),
-        Column('album_name', String),
-        Column('album_type', String),
-        Column('artist_id', String),
-        Column('release_date', String),
-        Column('total_tracks', Integer),
-        Column('load_date', DateTime, default=datetime.datetime.utcnow),
-         #schema='raw'
-        #schema?
-    )
-
-    # Load album data into the database
-
-    load(
-        df=album_df,
-        postgresql_client=postgresql_client,
-        table=albums_table,
-        metadata=metadata,
-        load_method="insert",
-    )
-
-
-
-
-    """
-    
-    playlist_dict = {
-        "SE": "7jmQBEvJyGHPqKEl5UcEe9",
-        "GB": "37i9dQZEVXbLnolsZ8PSNw",
-        "BE": "37i9dQZEVXbJNSeeHswcKB",
-        "USA":"37i9dQZEVXbLRQDuF5jeBp",
-        "PT":"37i9dQZEVXbKyJS56d1pgi"
-    }
-
-    """
-
-        
-    playlist_dict = {
-        "SE": "7jmQBEvJyGHPqKEl5UcEe9"
-    }
-    playlist_data = extract_global_playlist_countries(spotif_client,playlist_dict)
-    # print(playlist_data)  
-
-    df_playlist = transform_playlist(playlist_data)
-
-
-    playlist_global = Table(
-    'playlist', metadata,
-    Column('artist_id', String),
-    Column('playlist_country', String))
-
-
-    load(
-        df=df_playlist,
-        postgresql_client=postgresql_client,
-        table=playlist_global,
-        metadata=metadata,
-        load_method="insert",
-    )
-
-
+        pipeline_logging.logger.info("Pipeline run successful")
+        metadata_logger.log(
+            status=MetaDataLoggingStatus.RUN_SUCCESS, logs=pipeline_logging.get_logs()
+        )  # log end
+    except BaseException as e:
+        pipeline_logging.logger.error(f"Pipeline run failed. See detailed logs: {e}")
+        metadata_logger.log(
+            status=MetaDataLoggingStatus.RUN_FAILURE, logs=pipeline_logging.get_logs()
+        )  # log error
 
 
 
