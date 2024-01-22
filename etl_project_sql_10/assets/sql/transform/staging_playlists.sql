f1e6dc2026a202c6800a6b81f17c003920c73ca7
proteinBuffer <145110891+proteinBuffer@users.noreply.github.com>
2024-01-22 20:04:27 +0000

    Merge this work please! (#10)
    * adding logging
    
    * on testing
    
    * adding stuff
    
    ---------
    
    Co-authored-by: Gabrielle Klimovitsky <gabrielle@msk.ai>

--- etl_project_sql_10/assets/sql/transform/staging_playlists.sql
+++ etl_project_sql_10/assets/sql/transform/staging_playlists.sql
@@ -3,9 +3,9 @@ WITH staging_playlists AS (
         artist_id,
         LOWER(playlist_country) AS playlist_country_lower
     FROM
-        playlists
+        playlist
 )
 
 -- Use the CTE in your further queries if needed
 SELECT *
-FROM staging_playlists;
+FROM staging_playlists
